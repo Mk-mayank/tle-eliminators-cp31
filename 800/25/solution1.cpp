959a92daeaa16cad73712e97c6496453950fc2ef
Aniket Deore <aniketrdeore@gmail.com>
2024-11-23 19:03:23 +0530

    25

---
+++ 800/25/solution1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        
+        int left = 0, right = n - 1;
+        while (left < right && s[left] != s[right]) {
+            left++;
+            right--;
+        }
+        
+        int ans = max(0, right - left + 1);
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
